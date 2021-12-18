facd0eed8320c5d1aa5273ae82e8085e9b2fedf9
Ajay Karumathil <60084829+Ajayk10@users.noreply.github.com>
2021-12-18 10:02:08 +0530

    TodoList added

--- FinManApp/admin.py
+++ FinManApp/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from . models import *
 # Register your models here.
 
-classes = [JointAccountModel,CSBAccountModel,InHandModel,PensionAccountModel,MyAccountModel]
+classes = [JointAccountModel,CSBAccountModel,InHandModel,PensionAccountModel,MyAccountModel,TodoList]
 admin.site.register(classes)
